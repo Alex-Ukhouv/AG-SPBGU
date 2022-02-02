c2ddbbb94088e39c632b7371c8e183fc3fb7c322
Alex-Ukhouv <94801811+Alex-Ukhouv@users.noreply.github.com>
2022-02-02 14:34:11 +0300

    Add files via upload

---
+++ zadacha8.cpp
@@ -0,0 +1,34 @@
+﻿// zadacha8.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <stack>
+using namespace std;
+
+void dfs(int v)
+{
+    stack<int> stack;
+
+    stack.push(v);
+
+    while (!stack.empty())
+    {
+        v = stack.top();
+        stack.pop();
+
+        if (used[v]) {
+            continue;
+        }
+
+        used[v] = true;
+        cout << v << "\n";
+
+        for (auto i = first; i != last; i++)
+        {
+            int u = *i;
+            if (!used[u]) {
+                stack.push(u);
+            }
+        }
+    }
+}
